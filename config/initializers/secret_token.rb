9ff8fd49a2b8f33b825622fc70feaa4aa00669d7
Dino Kraljevic <ana.pintar1@gmail.com>
2011-05-17 10:56:06 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PrvaAplikacija::Application.config.secret_token = 'ffa7628287f6e175ecb92f37e534c07fff7a28759bbe0fcdd96c149a58535a5b30d04b8f936a8fe68fcfb6482053916f9210e7fa542239c8b583bcf78f87b731'
