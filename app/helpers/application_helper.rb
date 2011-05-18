2e0467d73665d62c0faa2eb18d45d6cc8402b6ed
Dino Kraljevic <ana.pintar1@gmail.com>
2011-05-18 23:01:59 +0200

    Added links and root paths

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,6 +8,11 @@ module ApplicationHelper
     else
       "#{base_title} | #{@title}"
     end
+  end 
+  def logo 
+    image_tag("logo.png", :alt => "Sample App", :class => "round")
   end
+  
 end
+
 
