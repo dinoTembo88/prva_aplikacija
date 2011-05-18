0dc8a978f83f507d5bca13ff31d85c2e3652884c
Dino Kraljevic <ana.pintar1@gmail.com>
2011-05-18 23:38:30 +0200

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 PrvaAplikacija::Application.routes.draw do
 
+    get "users/new"
+    match '/signup',  :to => 'users#new'
+
     root :to => "pages#home"
     
     match '/contact', :to => 'pages#contact'
